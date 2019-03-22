e37b3371411c8172d2c144968d915561e902683f
Cube219 <max804800@gmail.com>
2019-03-22 21:55:57 +0900

    Implement FrameAllocator
    Implement FrameAllocator which performs fast allocation and free.
    But the allocations will be discarded when next frame starts.

---
+++ Source/Core/EngineCore/Allocator/FrameAllocator.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+
+#include "../EngineCoreHeader.h"
+
+namespace cube
+{
+	class FrameAllocator
+	{
+	private:
+		class MemoryBlock
+		{
+		public:
+			MemoryBlock(Uint64 size);
+			~MemoryBlock();
+
+			void* Allocate(Uint64 size);
+			void* AllocateAligned(Uint64 size, Uint64 alignment);
+
+			void DiscardAllocations();
+
+		private:
+			Uint64 mSize;
+
+			void* mStartPtr;
+			void* mCurrentPtr;
+		};
+
+	public:
+		FrameAllocator(Uint64 blockSize = 5 * 1024 * 1024); // 0 MiB
+		~FrameAllocator();
+
+		void* Allocate(Uint64 size);
+		void Free(void* ptr);
+
+		void* AllocateAligned(Uint64 size, Uint64 alignment);
+		void FreeAligned(void* ptr);
+
+		void DiscardAllocations();
+
+	private:
+		void AllocateAdditioanlBlock(Uint64 size);
+
+		Uint64 mBlockSize;
+		MemoryBlock mMemoryBlock;
+
+		Vector<MemoryBlock> mAdditionalMemBlocks;
+
+#ifdef _DEBUG
+		Uint64 mAllocatedSize = 0;
+#endif // _DEBUG
+	};
+} // namespace cube
--- Source/Core/RenderAPI/RenderAPI/Utilities/VariableSizeMemoryPage.h
+++ Source/Core/RenderAPI/RenderAPI/Utilities/VariableSizeMemoryPage.h
@@ -1,19 +1,13 @@
 ﻿#pragma once
 
 #include "Base/BaseTypes.h"
+#include "EngineCore/Allocator/AllocatorUtility.h"
 #include "EngineCore/Assertion.h"
 
 namespace cube
 {
 	namespace render
 	{
-		template <typename T>
-		T Align(T offset, T alignment)
-		{
-			CHECK((alignment & (alignment - 1)) == 0, "Alignment({0}) must be power of 2.", alignment);
-			return (offset + (alignment - 1)) & ~(alignment - 1);
-		}
-
 		// Refer to VariableSizeAllocationsManager of DiligentEngine
 		// https://github.com/DiligentGraphics/DiligentEngine (VariableSizeAllocationsManager.h)
 
